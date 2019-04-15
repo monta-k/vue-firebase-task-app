4cb21f1c378f0d63a5d74494352449e6719a962e
Kobayashi Shunta <kobayashishunta@KobayashinoMacBook-Pro.local>
2019-04-15 15:24:22 +0900

    firebaseと連携

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue';
+import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+const config = {
+  apiKey: 'AIzaSyDvtjX1x12jUt9tMJS1vrOE_-QDVXxRnsk',
+  authDomain: 'task-app-ea9e8.firebaseapp.com',
+  databaseURL: 'https://task-app-ea9e8.firebaseio.com',
+  projectId: 'task-app-ea9e8',
+  storageBucket: 'task-app-ea9e8.appspot.com',
+  messagingSenderId: '938136653077',
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
