3ee478c558073e3ffb48b06eef64b511654dc964
Kobayashi Shunta <kobayashishunta@KobayashinoMacBook-Pro.local>
2019-04-16 17:42:01 +0900

    firebaseの設定をファイル分離

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyDvtjX1x12jUt9tMJS1vrOE_-QDVXxRnsk',
+  authDomain: 'task-app-ea9e8.firebaseapp.com',
+  databaseURL: 'https://task-app-ea9e8.firebaseio.com',
+  projectId: 'task-app-ea9e8',
+  storageBucket: 'task-app-ea9e8.appspot.com',
+  messagingSenderId: '938136653077',
+};
