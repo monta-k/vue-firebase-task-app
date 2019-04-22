c83a83baeedf9b2a92c56cdaecd6ea3ecd877918
Kobayashi Shunta <kobayashishunta@KobayashinoMacBook-Pro.local>
2019-04-22 14:26:40 +0900

    微調整

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue';
 import VeeValidate, { Validator } from 'vee-validate';
 import ja from 'vee-validate/dist/locale/ja';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router';
 
-library.add(fas, fab)
+library.add(fas, fab);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
